2b92a67bdf73dcc168db9f7f57dd2959edc5b322
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-07-11 17:25:53 -0300

    Atualização do projeto para utilizar expo-router, com adição de novas dependências e configuração do TypeScript. Criação de layout raiz e telas de aplicativo e login.

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { router } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <Text>Olá mundo!</Text>
+
+      <TouchableOpacity onPress={() => router.push('/login')}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
