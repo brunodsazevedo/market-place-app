e7bd6f0daf532d77f4e63ca60a47570a45f0e182
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-07-11 21:55:40 -0300

    Nenhuma alteração de código registrada.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Text, TouchableOpacity, View } from "react-native";
 export default function App() {
   return (
     <View>
-      <Text>Olá mundo!</Text>
+      <Text className="text-purple-base">Olá mundo!</Text>
 
       <TouchableOpacity onPress={() => router.push('/login')}>
         <Text>Login</Text>
