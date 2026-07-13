431f1f8c02b40562215381e65ddfacb1a8c6f61b
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-07-13 08:21:29 -0300

    atualizado o componente de layout para incluir telas de login e registro, e ajustada a versão da dependência react-native-worklets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,5 @@
-import { router } from 'expo-router'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Redirect } from 'expo-router'
 
 export default function App() {
-  return (
-    <View>
-      <Text className="text-purple-base">Olá mundo!</Text>
-
-      <TouchableOpacity onPress={() => router.push('/login')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
-  )
+  return <Redirect href="/login" />
 }
