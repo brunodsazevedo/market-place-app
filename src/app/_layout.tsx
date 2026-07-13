ed6dff1d13a71acc97781afd20f2fa8dcbb66c58
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-07-13 07:55:51 -0300

    adicionado suporte ao React Query no layout raiz

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import '@/styles/global.css'
 
+const queryClient = new QueryClient()
+
 export default function RootLayout() {
   return (
-    <Stack />
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
   )
 }
