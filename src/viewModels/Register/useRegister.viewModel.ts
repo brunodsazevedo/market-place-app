436558c440da2cfa22ed41870c4cae1d03c6f0bf
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-07-16 08:27:59 -0300

    adicionado suporte ao armazenamento assíncrono com AsyncStorage e implementada a gestão de sessão do usuário com Zustand

--- src/viewModels/Register/useRegister.viewModel.ts
+++ src/viewModels/Register/useRegister.viewModel.ts
@@ -3,10 +3,13 @@ import { yupResolver } from '@hookform/resolvers/yup'
 
 import { useRegisterMutation } from '@/shared/queries/auth/user-register.mutation'
 
+import { useUserStore } from '@/shared/store/user-store'
+
 import { RegisterFormData, registerScheme } from './register.scheme'
 
 export function useRegisterViewModel() {
   const userRegisterMutation = useRegisterMutation()
+  const { setSession } = useUserStore()
 
   const {
     control,
@@ -15,17 +18,23 @@ export function useRegisterViewModel() {
   } = useForm<RegisterFormData>({
     resolver: yupResolver(registerScheme),
     defaultValues: {
-      name: 'Bruno',
-      email: 'bruno-app@test.com',
+      name: 'Jose',
+      email: 'bruno-zustand@test.com',
       password: '123123',
       confirmPassword: '123123',
-      phone: '12999999999',
+      phone: '11999999999',
     },
   })
 
   const onSubmit = handleSubmit(async (userData) => {
     const { confirmPassword, ...registerData } = userData
-    await userRegisterMutation.mutateAsync(registerData)
+    const mutationResponse =
+      await userRegisterMutation.mutateAsync(registerData)
+    setSession({
+      user: mutationResponse.user,
+      token: mutationResponse.token,
+      refreshToken: mutationResponse.refreshToken,
+    })
   })
 
   return {
