9692c7380c45a3329d0241b937ff182810c7dc58
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-08-12 18:35:57 -0300

    Configurando BottomTabs

--- src/app/(private)/_layout.tsx
+++ src/app/(private)/_layout.tsx
@@ -8,5 +8,9 @@ export default function PrivateLayout() {
     return <Redirect href="/(public)/login" />
   }
 
-  return <Stack screenOptions={{ headerShown: false }} />
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
