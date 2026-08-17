ecf551f789e28c710fc56f9453644cfab475db0c
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2026-08-17 08:22:31 -0300

    Criando service de produtos

---
+++ src/shared/services/product.service.ts
@@ -0,0 +1,30 @@
+import { marketPlaceApiClient } from '@/shared/api/market-place-api'
+
+import { ProductResponse } from '@/shared/interfaces/http/product-response'
+
+interface ProductRequest {
+  pagination: {
+    page: number
+    perPage: number
+  }
+  filters: {
+    from: Date
+    to: Date
+    categoryIds: number[]
+    searchText: string
+    minValue: number
+    maxValue: number
+  }
+  sort: {
+    averageRating: string
+  }
+}
+
+export const getProducts = async (params: ProductRequest) => {
+  const { data } = await marketPlaceApiClient.post<ProductResponse>(
+    '/products',
+    params,
+  )
+
+  return data
+}
